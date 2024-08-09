3db39f21ef3df05bc6cc556707bd4a202d218d25
Vivek Kumar <vivekthakur1288@gmail.com>
2024-08-10 04:01:27 +0530

    bug fixed

--- JS/script.mjs
+++ JS/script.mjs
@@ -5,7 +5,6 @@ import products from "../products.json" with { type: "json" }
 let local_cart_products = localStorage.getItem("cart_items")
 let cart_products;
 
-location.reload()
 
 
 if(local_cart_products===null)
